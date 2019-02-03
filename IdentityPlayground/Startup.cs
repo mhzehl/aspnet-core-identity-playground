15914f7f60a611b7193a58f973e8648130476562
Melvin <melvin.zehl@s5.nl>
2019-02-03 17:48:08 +0100

    Update startup.cs with identityplaygrounduser and store.

--- IdentityPlayground/Startup.cs
+++ IdentityPlayground/Startup.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,7 +20,8 @@ namespace IdentityPlayground
         {
             services.AddMvc();
 
-            services.AddIdentityCore<string>(options => { });
+            services.AddIdentityCore<IdentityPlaygroundUser>(options => { });
+            services.AddScoped<IUserStore<IdentityPlaygroundUser>, IdentityPlayGroundUserStore>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
